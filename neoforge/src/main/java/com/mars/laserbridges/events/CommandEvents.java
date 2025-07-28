19243d8d04523139f63c876895a049b4a2bcf4e7
Doggiedude1 <eli.hoestje@gmail.com>
2025-07-28 10:55:57 -0500

    Fix decoy command registration

---
+++ neoforge/src/main/java/com/mars/laserbridges/events/CommandEvents.java
@@ -0,0 +1,18 @@
+package com.mars.laserbridges.events;
+
+import com.mars.laserbridges.commands.DecoyCommand;
+import com.mojang.brigadier.CommandDispatcher;
+import net.minecraft.commands.CommandSourceStack;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.common.EventBusSubscriber;
+import static com.mars.laserbridges.Constants.MOD_ID;
+import net.neoforged.neoforge.event.RegisterCommandsEvent;
+
+@EventBusSubscriber(modid = MOD_ID)
+public class CommandEvents {
+    @SubscribeEvent
+    public static void onRegisterCommands(RegisterCommandsEvent event) {
+        CommandDispatcher<CommandSourceStack> dispatcher = event.getDispatcher();
+        DecoyCommand.register(dispatcher);
+    }
+}
